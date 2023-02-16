d2da1cbb1d1095e54798235963b3322d066d5aa1
Mike Pronin <mr.mi@mail.ru>
2023-02-17 00:27:38 +0400

    Задача №15

--- Task013/Program.cs
+++ Task013/Program.cs
@@ -5,18 +5,18 @@
 // ДОП: цифра третья может быть как с левой стороны, так и с правой
 
 Console.WriteLine("Введите число: ");
-if (! int.TryParse(Console.ReadLine(), out int number))
-    {
-        Console.WriteLine("Вы ввели не число");
-    }
+if (!int.TryParse(Console.ReadLine(), out int number))
+{
+  Console.WriteLine("Вы ввели не число");
+}
 //int number = Convert.ToInt32(Console.ReadLine());
-int resultRight = ((number % 100000) - (number % 10000)) / 10000;
-int resultLeft = ((number % 1000) - (number % 100)) / 100;
+int resultLeft = ((number % 100000) - (number % 10000)) / 10000;
+int resultRight = ((number % 1000) - (number % 100)) / 100;
 if (number >= 1000000 && number <= 9999999)
 {
-  Console.WriteLine($"Третья цифра с начала числа: {resultRight}, а третья цифра с конца числа {resultLeft}");
+  Console.WriteLine($"Третья цифра с лева: {resultLeft}, а третья цифра с права {resultRight}");
 }
-if (number >= 1 && number <100)
+if (number >= 1 && number < 100)
 {
   Console.WriteLine("Третьей цифры нет");
 }
