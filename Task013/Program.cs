c8852b3393f1072f3e663df9c671f5a23e271a1f
Mike Pronin <mr.mi@mail.ru>
2023-02-17 00:07:35 +0400

    Задача №13

---
+++ Task013/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного семизначного 
+// числа или сообщает, что третьей цифры нет.
+// 78 -> третьей цифры нет
+// 3267900 -> 6
+// ДОП: цифра третья может быть как с левой стороны, так и с правой
+
+Console.WriteLine("Введите число: ");
+if (! int.TryParse(Console.ReadLine(), out int number))
+    {
+        Console.WriteLine("Вы ввели не число");
+    }
+//int number = Convert.ToInt32(Console.ReadLine());
+int resultRight = ((number % 100000) - (number % 10000)) / 10000;
+int resultLeft = ((number % 1000) - (number % 100)) / 100;
+if (number >= 1000000 && number <= 9999999)
+{
+  Console.WriteLine($"Третья цифра с начала числа: {resultRight}, а третья цифра с конца числа {resultLeft}");
+}
+if (number >= 1 && number <100)
+{
+  Console.WriteLine("Третьей цифры нет");
+}
+if (number > 9999999)
+{
+  Console.WriteLine("Введено некорректное число");
+}
